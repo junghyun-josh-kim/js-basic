8641ceed8e5909db17aa6e5eb45ae378671191ce
Josh-Kim-Toronto <jhkim911206@gmail.com>
2022-09-24 01:22:04 -0400

    Create 3.quiz.js

---
+++ 3.quiz.js
@@ -0,0 +1,15 @@
+let num = 2;
+//num 짝수 "!" 홀수 "?"
+
+//if
+
+//ternary
+
+
+if (num % 2 == 0) {
+    console.log("!")
+} else {
+    console.log("?")
+}
+
+num % 2 == 0 ? console.log("!") : console.log("?")
